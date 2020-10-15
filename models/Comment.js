91a69635a137751b6be06497519352e0b9cf834f
IA Masum <38923620+IA-Masum@users.noreply.github.com>
2020-10-15 23:15:01 +0600

    Models Designe completed

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+const {Schema, model} = require('mongoose')
+
+
+const User = require('./User');
+const Post = require('./Post');
+
+const commentSchema = new Schema({
+    post:{
+        type: Schema.Types.ObjectId,
+        ref: Post,
+        required: true
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    body:{
+        type: String,
+        maxlength: 300,
+        trim: true,
+        required: true
+    },
+    replies:[
+        {
+            body: {
+                type: String,
+                maxlength: 300,
+                trim: true,
+                required: true
+            },
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: User,
+                required: true
+            },
+            createdAt:{
+                type: Date,
+                default: new Date()
+            }
+        }
+    ]
+
+})
+
+
+const Comment  = model('Comment', commentSchema)
+
+module.exports = Comment
